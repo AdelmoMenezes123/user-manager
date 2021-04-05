8a902485123105e77f71084e63e2429c740bfd9b
Adelmo Menezes <adelmo.menezes2010@gmail.com>
2021-04-05 16:22:42 -0300

    cria model de usuario

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ var fields = document.querySelectorAll('#form-user-create [name]');
 var user = {};
 
 function addLine(dataUser) {
-    var tr = document.createElement("tr");
 
-    tr.innerHTML = `
+    document.getElementById("table-users").innerHTML = `
     <tr>
         <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
         <td>${dataUser.name}</td>
@@ -17,8 +16,6 @@ function addLine(dataUser) {
         </td>
     </tr>
     `;
-    document.getElementById('table-users').appendChild(tr)
-
 }
 
 document.getElementById('form-user-create').addEventListener('submit', function (event) {
@@ -35,6 +32,18 @@ document.getElementById('form-user-create').addEventListener('submit', function 
         }
     });
 
-    addLine(user)
+    var objectUser = new Users(
+        user.name,
+        user.gender,
+        user.birth,
+        user.country,
+        user.email,
+        user.password,
+        user.photo,
+        user.admin
+    );
+
+
+    addLine(objectUser)
 
 });
---
+++ models/User.js
@@ -0,0 +1,13 @@
+class Users {
+
+    constructor(name, gender, birth, email, password, country, photo, admin) {
+        this.name = name;
+        this.gender = gender;
+        this.birth = birth;
+        this.email = email;
+        this.password = password;
+        this.country = country;
+        this.photo = photo;
+        this.admin = admin;
+    }
+}
