27e6eff011f98fa5d4519f1a606cd41fb263d7bc
Adelmo Menezes <adelmo.menezes2010@gmail.com>
2021-06-05 17:15:00 -0300

    corrige erro de  file

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -46,7 +46,11 @@ class UserController {
                 reject(e)
             }
 
-            fileReader.readAsDataURL(file);
+            if(file){
+                fileReader.readAsDataURL(file);
+            }else{
+                resolve('dist/img/boxed-bg.jpg');
+            }
 
         })
     }
@@ -60,7 +64,9 @@ class UserController {
                 if (field.checked) {
                     user[field.name] = field.value;
                 }
-            } else {
+            } else if(field.name == 'admin'){
+                user[field.name] = field.checked;
+            }else {
                 user[field.name] = field.value;
             }
         });
@@ -79,12 +85,13 @@ class UserController {
 
     addLine(dataUser) {
 
-        this.tableEl.innerHTML = `
+        let tr = document.createElement('tr');
+        tr.innerHTML = `
         <tr>
             <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
             <td>${dataUser.name}</td>
             <td>${dataUser.email}</td>
-            <td>${dataUser.admin}</td>
+            <td>${(dataUser.admin)? 'Sim' : 'Não' }</td>
             <td>${dataUser.birth}</td>
             <td>
                 <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
@@ -92,5 +99,7 @@ class UserController {
             </td>
         </tr>
         `;
+
+        this.tableEl.appendChild(tr)
     }
 }
