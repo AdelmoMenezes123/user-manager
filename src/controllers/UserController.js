98e6ed101f9fdf2732e8780782b8cf3d87a58f54
Adelmo Menezes <adelmo.menezes2010@gmail.com>
2021-07-08 11:35:04 -0300

    retorna os dados ao formulario de editar

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -132,7 +132,18 @@ class UserController {
         `;
 
         tr.querySelector('.btn-edit').addEventListener("click", (e) => {
-            // JSON.parse(tr.dataset.user)
+            let json = JSON.parse(tr.dataset.user);
+            let form = document.querySelector("#form-user-update");
+            for (let name in json) {
+                let field = form.querySelector(`[name="${name.replace('_', '')}"]`)
+
+
+                if (field) {
+                    if (field.type == 'file') { continue };
+                    field.value = json[name];
+                }
+            }
+
             this.showPanelUpdate();
         })
 
