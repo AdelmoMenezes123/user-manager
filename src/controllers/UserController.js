f169be1b5ff2c6acfa004e6b553ffbd0abcd870d
Adelmo Menezes <adelmo.menezes2010@gmail.com>
2021-07-08 13:23:44 -0300

    metodo para remover registro

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -49,7 +49,7 @@ class UserController {
                             <td>${Ultils.dateFormat(result._register)}</td>
                             <td>
                                 <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-                                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                                <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
                             </td>
                         </tr>
                     `;
@@ -185,7 +185,7 @@ class UserController {
             <td>${Ultils.dateFormat(dataUser.register)}</td>
             <td>
                 <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
             </td>
         </tr>
         `;
@@ -197,6 +197,16 @@ class UserController {
     }
 
     addEventesTr(tr) {
+
+        tr.querySelector('.btn-delete').addEventListener("click", (e) => {
+            if(confirm("Deseja realmente excluir?")){
+                tr.remove();
+                
+                this.updateCount();
+            }
+        });
+
+
         tr.querySelector('.btn-edit').addEventListener("click", (e) => {
             let json = JSON.parse(tr.dataset.user);
 
